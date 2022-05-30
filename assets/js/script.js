a9741355f94cde235c9221b9dfa77c2ba40b910b
hackpres <102390515+hackpres@users.noreply.github.com>
2022-05-30 16:21:27 -0400

    added api request to pull lat and lon of user input city.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,26 @@
 
+const citySearch = document.getElementById("citySearchInput");
 const searchBtn = document.getElementById("searchBtn");
+const previousHistoryBtn = document.getElementById("previousHistoryBtn");
+
+// previousHistoryBtn.addEventListener('click', searchHistory());
+citySearch.addEventListener('change', () => {
+    getWeather(citySearch.innerText);
+    console.log(citySearch.value);
+})
+searchBtn.addEventListener('click', () => {
+    var userCity = citySearch.value
+    console.log(userCity);
+    getWeather(userCity);
+})
 
 
-function getWeather() {
-    var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?q=atlanta&appid=cccd45bf2b5eca98c58877bde4b85aed';
-    console.log(requestUrl);
-    fetch(requestUrl, {
+function getWeather(userCityInput) {
+    var city = userCityInput;
+    console.log('city' + city);
+    var requestCity = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&&appid=cccd45bf2b5eca98c58877bde4b85aed`;
+    console.log(requestCity);
+    fetch(requestCity, {
         method: "GET",
         credentials: 'same-origin'
     })
@@ -16,6 +31,15 @@ function getWeather() {
         .then(function (data) {
             console.log('data received')
             console.log(data);
+            var cityLon = data[0].lon;
+            var cityLat = data[0].lat;
+            console.log(cityLat, cityLon);
         });
+
 }
 
+// console.log(`getWeather ${getWeather()}`);
+
+// function searchHistory() {
+
+// }
